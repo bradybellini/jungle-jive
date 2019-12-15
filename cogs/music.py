a8c740e53eda12a562023c2086a92210cd0ae0f0
bradybellini <bellinibrady65@gmail.com>
2019-12-15 13:14:40 -0700

    Update music.py

--- cogs/music.py
+++ cogs/music.py
@@ -9,6 +9,7 @@ class Music(commands.Cog, name="Please don't stop the music"):
     def __init__(self, client):
         self.client = client
     
-    @commands.commnd()
-    aysnc def ping(self, ctx, ping=none):
-        pass
+    @commands.command(aliases=['history'])
+    async def gethistory(self, ctx, ping=None):
+        async for message in client.channel.history(limit=1000):
+            if message
