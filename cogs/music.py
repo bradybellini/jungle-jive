17c865ccca07fcb77ec8a01138bd18cfe0c2e925
bradybellini <bellinibrady65@gmail.com>
2019-12-15 13:03:43 -0700

    Update music.py

--- cogs/music.py
+++ cogs/music.py
@@ -4,8 +4,11 @@ import re
 from discord import Message, TextChannel, Member, PartialEmoji
 from discord.ext import commands
 
-class Music(commands.cog, name="Please don't stop the music"):
+class Music(commands.Cog, name="Please don't stop the music"):
 
     def __init__(self, client):
         self.client = client
-
+    
+    @commands.commnd()
+    aysnc def ping(self, ctx, ping=none):
+        pass
