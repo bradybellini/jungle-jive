55bb3b514c8902415ef45602b4aa034beb89921d
Brady <bellinibrady65@gmail.com>
2019-12-17 17:21:18 -0700

    used to create db

--- create_db.py
+++ create_db.py
@@ -3,6 +3,6 @@ import sqlite3
 conn = sqlite3.connect('music.db') 
 c = conn.cursor()
 c.execute('''CREATE TABLE links
-             ([link TEXT UNIQUE,[date_posted] TEXT, [author] TEXT, [jump_link] TEXT)''')
+             ([link] TEXT UNIQUE,[date_posted] TEXT, [author] TEXT, [jump_link] TEXT, [count] INTEGER)''')
 
 conn.commit()
